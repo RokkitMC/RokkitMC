0c76f7aae113c81d533fde1cde9be4161d1b85d8
Alex Howe <mail@ahowe.dev>
2020-12-05 23:11:08 +0000

    Converted RokkitMC to a shared object, hooked function

--- include/definitions.h
+++ include/definitions.h
@@ -5,25 +5,25 @@
 #ifndef ROKKITMC_DEFINITIONS_H
 #define ROKKITMC_DEFINITIONS_H
 #include <stdint.h>
-
-typedef int64_t*(__fastcall* playerJoined_t)(int64_t* leaderboard, int64_t plr);
-playerJoined_t onPlayerJoinedO;
-
-typedef void(__fastcall* clientAuthenticated_t)(int64_t, int64_t, int64_t);
-clientAuthenticated_t onClientAuthenticatedO;
-
-int64_t _handleBase;
-
-int64_t offset(int64_t x)
-{
-    return ((x) - 0x140000000 + (_handleBase));
-}
-
-void DefineTypedefs(int64_t handleBase)
-{
-    _handleBase = handleBase;
-    onPlayerJoinedO = reinterpret_cast<playerJoined_t>(offset(0x1411E5B70));
-    onClientAuthenticatedO = reinterpret_cast<clientAuthenticated_t>(offset(0x140993090));
-}
+//
+//typedef int64_t*(__fastcall* playerJoined_t)(int64_t* leaderboard, int64_t plr);
+//playerJoined_t onPlayerJoinedO;
+//
+//typedef void(__fastcall* clientAuthenticated_t)(int64_t, int64_t, int64_t);
+//clientAuthenticated_t onClientAuthenticatedO;
+//
+//int64_t _handleBase;
+//
+//int64_t offset(int64_t x)
+//{
+//    return ((x) - 0x140000000 + (_handleBase));
+//}
+//
+//void DefineTypedefs(int64_t handleBase)
+//{
+//    _handleBase = handleBase;
+//    onPlayerJoinedO = reinterpret_cast<playerJoined_t>(offset(0x1411E5B70));
+//    onClientAuthenticatedO = reinterpret_cast<clientAuthenticated_t>(offset(0x140993090));
+//}
 
 #endif //ROKKITMC_DEFINITIONS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
-#include "subhook.h"
+#include <dlfcn.h>
+#include <subhook.h>
 
 using std::cout;
 using std::string;
+using subhook::Hook;
 
-int main() {
+subhook_t clientAuthHook;
+
+typedef void(*__fastcall clientAuthenticated_t)(int64_t, int64_t, int64_t);
+
+void clientAuthenticated(int64_t* ni, int64_t* cert) {
+	printf("Client Authenticated!");
+
+	void* trampoline = subhook_get_trampoline(clientAuthHook);
+	auto original = (clientAuthenticated_t)trampoline;
+	original(ni, cert);
+}
+
+__attribute__((constructor))
+void rokkitmc_init() {
 	string version = "1.16.10";
+	printf("Loading RokkitMC for BDS: %s!\n", version.c_str());
 
-	cout << "Starting Bedrock Dedicated Server: " << version << "\n";
+	void* ptr = dlsym(RTLD_DEFAULT, "_ZN20ServerNetworkHandler22_onClientAuthenticatedERK17NetworkIdentifierRK11Certificate");
 
+	if(!ptr) {
+		printf(":(\n");
+		return;
+	}
 
+	void* clientAuth = (void*) clientAuthenticated;
+	clientAuthHook = subhook_new(ptr, clientAuth, static_cast<subhook_flags>(0));
 
-	cout << "Stopping Bedrock Dedicated Server\n";
-	return 0;
+	int rc = subhook_install(clientAuthHook);
+
+	printf("Subhook Install: %d\n", rc);
+}
+
+__attribute__((destructor))
+void rokkitmc_deinit() {
+	subhook_remove(clientAuthHook);
+	subhook_free(clientAuthHook);
 }

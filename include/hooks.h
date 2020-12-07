33bfdbc0842f2f72181751f375d3baf935ff93e2
TatoExp <mwmatthew10@gmail.com>
2020-12-07 13:08:51 +0000

    IDEK

---
+++ include/hooks.h
@@ -0,0 +1,8 @@
+//
+// Created by mwmat on 07/12/2020.
+//
+
+#ifndef ROKKITMC_HOOKS_H
+#define ROKKITMC_HOOKS_H
+
+#endif //ROKKITMC_HOOKS_H
---
+++ include/logger.h
@@ -0,0 +1,8 @@
+//
+// Created by mwmat on 07/12/2020.
+//
+
+#ifndef ROKKITMC_LOGGER_H
+#define ROKKITMC_LOGGER_H
+
+#endif //ROKKITMC_LOGGER_H

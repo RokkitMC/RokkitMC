9a10e876a9806b6815591d44b2c940ff96fb3ce3
TatoExp <mwmatthew10@gmail.com>
2020-12-07 13:41:30 +0000

    added changes

--- include/hooks.h
+++ include/hooks.h
@@ -4,5 +4,18 @@
 
 #ifndef ROKKITMC_HOOKS_H
 #define ROKKITMC_HOOKS_H
+#include <iostream>
+#include <list>
+#include <subhook.h>
 
+std::list<subhook_t> activeHooks;
+
+subhook_t Hook (void** original, void* hook, const char* funcName) {
+    *original = dlsym(RTLD_DEFAULT, funcName);
+    subhook_t newHook = subhook_new(*original, hook, static_cast<subhook_flags>(0));
+    int rc = subhook_install(newHook);
+    if(rc != 0)
+        printf("Error hooking %s", funcName);
+    return newHook;
+}
 #endif //ROKKITMC_HOOKS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <dlfcn.h>
-#include <subhook.h>
+#include <thread>
+#include <unistd.h>
+#include "hooks.h"
+#include "logger.h"
 
 using std::cout;
 using std::string;
@@ -12,34 +15,32 @@ void* clientAuthOrig;
 typedef uint64_t(*clientAuthenticated_t)(int64_t*, int64_t*, int64_t*);
 
 uint64_t clientAuthenticated(int64_t* self, int64_t* ni, int64_t* cert) {
-	printf("Client Authenticated!\n");
-	subhook_remove(clientAuthHook);
-	auto original = (clientAuthenticated_t)clientAuthOrig;
-	auto result = original(self, ni, cert);
-	subhook_install(clientAuthHook);
-	return result;
+    printf("Client Authenticated!\n");
+    subhook_remove(clientAuthHook);
+    auto original = (clientAuthenticated_t)clientAuthOrig;
+    auto result = original(self, ni, cert);
+    subhook_install(clientAuthHook);
+    return result;
+}
+__attribute((constructor))
+void entry() {
+    string version = "1.16.10";
+    printf("Loading RokkitMC for BDS: %s!\n", version.c_str());
+    clientAuthHook = Hook(&clientAuthOrig, (void*)clientAuthenticated, "_ZN20ServerNetworkHandler22_onClientAuthenticatedERK17NetworkIdentifierRK11Certificate");
+    printf("%d %d %d", clientAuthOrig, clientAuthenticated, clientAuthHook);
 }
 
-__attribute__((constructor))
-void rokkitmc_init() {
-	string version = "1.16.10";
-	printf("Loading RokkitMC for BDS: %s!\n", version.c_str());
-    clientAuthOrig = dlsym(RTLD_DEFAULT, "_ZN20ServerNetworkHandler22_onClientAuthenticatedERK17NetworkIdentifierRK11Certificate");
-	if(!clientAuthOrig) {
-		printf(":(\n");
-		return;
-	}
-
-	void* clientAuth = (void*) clientAuthenticated;
-	clientAuthHook = subhook_new(clientAuthOrig, clientAuth, static_cast<subhook_flags>(0));
-
-	int rc = subhook_install(clientAuthHook);
-
-	printf("Subhook Install: %d\n", rc);
-}
-
-__attribute__((destructor))
-void rokkitmc_deinit() {
-	subhook_remove(clientAuthHook);
-	subhook_free(clientAuthHook);
-}
+//static struct init {
+//    init()
+//    {
+//        entry();
+//    }
+//    ~init()
+//    {
+//        std::list<subhook_t>::iterator it;
+//        for (it = activeHooks.begin(); it != activeHooks.end(); ++it) {
+//            subhook_remove(*it);
+//            subhook_free(*it);
+//        }
+//    }
+//} init;

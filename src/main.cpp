ab6e5953b19b0565e708e86d8e2b40e858f2e1ac
TatoExp <mwmatthew10@gmail.com>
2020-12-07 11:40:31 +0000

    got hook to work

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,31 @@ using std::string;
 using subhook::Hook;
 
 subhook_t clientAuthHook;
+void* clientAuthOrig;
 
-typedef void(*__fastcall clientAuthenticated_t)(int64_t, int64_t, int64_t);
+typedef uint64_t(*clientAuthenticated_t)(int64_t*, int64_t*, int64_t*);
 
-void clientAuthenticated(int64_t* ni, int64_t* cert) {
-	printf("Client Authenticated!");
-
-	void* trampoline = subhook_get_trampoline(clientAuthHook);
-	auto original = (clientAuthenticated_t)trampoline;
-	original(ni, cert);
+uint64_t clientAuthenticated(int64_t* self, int64_t* ni, int64_t* cert) {
+	printf("Client Authenticated!\n");
+	subhook_remove(clientAuthHook);
+	auto original = (clientAuthenticated_t)clientAuthOrig;
+	auto result = original(self, ni, cert);
+	subhook_install(clientAuthHook);
+	return result;
 }
 
 __attribute__((constructor))
 void rokkitmc_init() {
 	string version = "1.16.10";
 	printf("Loading RokkitMC for BDS: %s!\n", version.c_str());
-
-	void* ptr = dlsym(RTLD_DEFAULT, "_ZN20ServerNetworkHandler22_onClientAuthenticatedERK17NetworkIdentifierRK11Certificate");
-
-	if(!ptr) {
+    clientAuthOrig = dlsym(RTLD_DEFAULT, "_ZN20ServerNetworkHandler22_onClientAuthenticatedERK17NetworkIdentifierRK11Certificate");
+	if(!clientAuthOrig) {
 		printf(":(\n");
 		return;
 	}
 
 	void* clientAuth = (void*) clientAuthenticated;
-	clientAuthHook = subhook_new(ptr, clientAuth, static_cast<subhook_flags>(0));
+	clientAuthHook = subhook_new(clientAuthOrig, clientAuth, static_cast<subhook_flags>(0));
 
 	int rc = subhook_install(clientAuthHook);
 

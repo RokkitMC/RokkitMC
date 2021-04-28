b422aa5d6e7a96495ed57661bf9fe75dfc783e3c
Alex Howe <mail@ahowe.dev>
2021-04-28 09:00:54 +0100

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "logger.h"
 #include "Rokkit/Rokkit.h"
 #include "hooks.h"
-#include "PluginLoader.h"
 #include <unordered_map>
 
 using std::cout, std::string, std::endl;
@@ -46,33 +45,12 @@ uint64_t clientAuthenticated(int64_t* self, int64_t* ni, Rokkit::Certificate* ce
 
 typedef uint64_t(*recordEvent_t)(int64_t*, Rokkit::Event*);
 
-typedef std::string(*getName_t)(int64_t*);
-
-uint64_t recordEvent(int64_t* self, Rokkit::Event* event) {
-    Logger::Info("Event fired\n");
-    //cout << event->propertiesAsJsonValue()->toStyledString() << endl;
-    std::unordered_map<string, int64_t*> properties = *(std::unordered_map<string, int64_t*>*)(event+64);
-    for (std::pair<std::string, int64_t*> element : properties)
-    {
-        std::cout << element.first << " :: " << element.second << std::endl;
-    }
-
-    subhook_remove(recordEventHook);
-    auto original = (recordEvent_t)recordEventOriginal;
-    auto result = original(self, event);
-    subhook_install(recordEventHook);
-    Logger::Info("Success\n");
-    return result;
-}
-
 void entry() {
     string version = "1.16.20";
     printf("%s[RokkitMC]:%s Loading RokkitMC for BDS: %s!\n", GREEN, WHITE, version.c_str());
     clientAuthHook = Hook(&clientAuthOrig, (void*)clientAuthenticated, "_ZN20ServerNetworkHandler22_onClientAuthenticatedERK17NetworkIdentifierRK11Certificate");
     playerMessageHook = Hook(&playerMessageOrig, (void*) playerMessage, "_ZN17MinecraftEventing22fireEventPlayerMessageERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEES7_S7_S7_");
-    recordEventHook = Hook(&recordEventOriginal, (void*) recordEvent, "_ZN6Social6Events12EventManager11recordEventERNS0_5EventE");
     //Do JVM init last
-    Rokkit::PluginLoader* pluginLoader = new Rokkit::PluginLoader(JNI_VERSION_1_8);
 }
 
 static struct init {
